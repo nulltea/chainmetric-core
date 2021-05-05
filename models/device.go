1fb0559c5c481141e0da8a8fe23ee7fa74e48503
Timothy <timauthx@gmail.com>
2021-05-06 00:40:56 +0300

    Add commands register in device model, define some basic commands

--- models/device.go
+++ models/device.go
@@ -6,22 +6,34 @@ import (
 	"github.com/pkg/errors"
 )
 
-// Device defines device data models
+// Device defines device data models.
 type Device struct {
-	ID       string      `json:"id"`
-	IP       string      `json:"ip"`
-	MAC      string      `json:"mac,omitempty" metadata:",optional"`
-	Name     string      `json:"name,omitempty" metadata:",optional"`
-	Hostname string      `json:"hostname"`
-	Profile  string      `json:"profile,omitempty" metadata:",optional"`
-	Supports Metrics     `json:"supports"`
-	Holder   string      `json:"holder"`
-	State    DeviceState `json:"state,omitempty" metadata:",optional"`
-	Location string      `json:"location"`
+	ID       string          `json:"id"`
+	IP       string          `json:"ip"`
+	MAC      string          `json:"mac,omitempty" metadata:",optional"`
+	Name     string          `json:"name,omitempty" metadata:",optional"`
+	Hostname string          `json:"hostname"`
+	Profile  string          `json:"profile,omitempty" metadata:",optional"`
+	Supports Metrics         `json:"supports"`
+	Holder   string          `json:"holder"`
+	State    DeviceState     `json:"state,omitempty" metadata:",optional"`
+	Location string          `json:"location"`
+	Commands []DeviceCommand `json:"commands"`
 }
 
+// DeviceState defines Device states enum.
 type DeviceState string
 
+// DeviceCommand defines Device commands enum.
+type DeviceCommand string
+
+const (
+	Pause DeviceCommand = "pause"
+	Resume DeviceCommand = "resume"
+	PairBluetooth DeviceCommand = "ble_pair"
+)
+
+// Encode serializes the Device model.
 func (m Device) Encode() []byte {
 	data, err := json.Marshal(m); if err != nil {
 		return nil
@@ -29,11 +41,13 @@ func (m Device) Encode() []byte {
 	return data
 }
 
+// Decode serializes the Device model.
 func (m Device) Decode(b []byte) (*Device, error) {
 	err := json.Unmarshal(b, &m)
 	return &m, err
 }
 
+// Decode validates the Device model.
 func (m *Device) Validate() error {
 	if len(m.ID) == 0 {
 		return errors.New("id must be assigned to device")
