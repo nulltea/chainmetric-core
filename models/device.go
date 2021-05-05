e75d845d6a7ff65a492e07e93a1af9bbf1328878
Timothy <timauthx@gmail.com>
2021-05-06 02:06:54 +0300

    Add device command execution log entry model and status enum

--- models/device.go
+++ models/device.go
@@ -2,48 +2,24 @@ package models
 
 import (
 	"encoding/json"
+	"time"
 
 	"github.com/pkg/errors"
 )
 
 // Device defines device data models.
 type Device struct {
-	ID       string          `json:"id"`
-	IP       string          `json:"ip"`
-	MAC      string          `json:"mac,omitempty" metadata:",optional"`
-	Name     string          `json:"name,omitempty" metadata:",optional"`
-	Hostname string          `json:"hostname"`
-	Profile  string          `json:"profile,omitempty" metadata:",optional"`
-	Supports Metrics         `json:"supports"`
-	Holder   string          `json:"holder"`
-	State    DeviceState     `json:"state,omitempty" metadata:",optional"`
-	Location string          `json:"location"`
+	ID       string      `json:"id"`
+	IP       string      `json:"ip"`
+	MAC      string      `json:"mac,omitempty" metadata:",optional"`
+	Name     string      `json:"name,omitempty" metadata:",optional"`
+	Hostname string      `json:"hostname"`
+	Profile  string      `json:"profile,omitempty" metadata:",optional"`
+	Supports Metrics     `json:"supports"`
+	Holder   string      `json:"holder"`
+	State    DeviceState `json:"state,omitempty" metadata:",optional"`
+	Location string      `json:"location"`
 }
-
-// DeviceState defines Device states enum.
-type DeviceState string
-
-var (
-	// DeviceOnline defines Device "online" state.
-	DeviceOnline DeviceState = "online"
-	// DeviceOffline defines Device "offline" state.
-	DeviceOffline DeviceState = "offline"
-	// DevicePaused defines Device "paused" state.
-	DevicePaused DeviceState = "paused"
-)
-
-
-// DeviceCommand defines Device commands enum.
-type DeviceCommand string
-
-const (
-	// DevicePauseCmd defines DeviceCommand to "pause" Device.
-	DevicePauseCmd DeviceCommand = "pause"
-	// DevicePauseCmd defines DeviceCommand to "pause" Device.
-	DeviceResumeCmd DeviceCommand = "resume"
-	// DevicePairingCmd defines DeviceCommand to pair Device via Bluetooth.
-	DevicePairingCmd DeviceCommand = "ble_pair"
-)
 
 // Encode serializes the Device model.
 func (m Device) Encode() []byte {
@@ -75,3 +51,51 @@ func (m *Device) Validate() error {
 
 	return nil
 }
+
+
+// DeviceState defines Device states enum.
+type DeviceState string
+
+var (
+	// DeviceOnline defines Device "online" state.
+	DeviceOnline DeviceState = "online"
+	// DeviceOffline defines Device "offline" state.
+	DeviceOffline DeviceState = "offline"
+	// DevicePaused defines Device "paused" state.
+	DevicePaused DeviceState = "paused"
+)
+
+
+// DeviceCommand defines Device commands enum.
+type DeviceCommand string
+
+const (
+	// DevicePauseCmd defines DeviceCommand to "pause" Device.
+	DevicePauseCmd DeviceCommand = "pause"
+	// DevicePauseCmd defines DeviceCommand to "pause" Device.
+	DeviceResumeCmd DeviceCommand = "resume"
+	// DevicePairingCmd defines DeviceCommand to pair Device via Bluetooth.
+	DevicePairingCmd DeviceCommand = "ble_pair"
+)
+
+// DeviceCommandLogEntry defines DeviceCommand execution historical log record.
+type DeviceCommandLogEntry struct {
+	DeviceID  string              `json:"device_id"`
+	Command   DeviceCommand       `json:"command"`
+	Args      DeviceCommand       `json:"args"`
+	Status    DeviceCommandStatus `json:"status"`
+	Error     string              `json:"err"`
+	Timestamp time.Time           `json:"timestamp"`
+}
+
+// DeviceCommandStatus defines DeviceCommand execution status.
+type DeviceCommandStatus string
+
+const (
+	// DeviceCmdCompleted defines "completed" status of DeviceCommand execution.
+	DeviceCmdCompleted DeviceCommandStatus = "completed"
+	// DeviceCmdProcessing defines "processing" status of DeviceCommand execution.
+	DeviceCmdProcessing DeviceCommandStatus = "processing"
+	// DeviceCmdFailed defines "failed" status of DeviceCommand execution.
+	DeviceCmdFailed DeviceCommandStatus = "failed"
+)
