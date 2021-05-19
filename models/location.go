b9ac4728afa1e1b0759583e1ee3f395fba081bf4
Timothy <timauthx@gmail.com>
2021-05-19 21:14:13 +0300

    Update geo location comparing methods to use meters

--- models/location.go
+++ models/location.go
@@ -11,12 +11,14 @@ type Location struct {
 	Longitude float64
 }
 
+// IsNearBy determines whether two geo points is located within `d` distance radius (m).
 func (l Location) IsNearBy(l2 Location, d float64) bool {
-	return l.toGeoPoint().GreatCircleDistance(l2.toGeoPoint()) <= d
+	return l.Distance(l2) * 1000 <= d
 }
 
+// Distance calculates distance between two geo points (in m).
 func (l Location) Distance(l2 Location) float64 {
-	return l.toGeoPoint().GreatCircleDistance(l2.toGeoPoint())
+	return l.toGeoPoint().GreatCircleDistance(l2.toGeoPoint()) * 1000
 }
 
 func (l Location) toGeoPoint() *geo.Point {
